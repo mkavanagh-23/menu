163ee8625b6ebf48b2275c234dd0e5b4aac1430c
Matthew Kavanagh <matt@matthewkavanagh.us>
2025-01-20 13:59:58 -0500

    Fix zenmode binding again

--- lua/menus/default.lua
+++ lua/menus/default.lua
@@ -26,7 +26,7 @@ return {
       -- Get the dimensions of the current window and calculate the center position
       local width = vim.o.columns
       local height = vim.o.lines
-      local win_width = 80 -- Width of the floating window
+      local win_width = 80  -- Width of the floating window
       local win_height = 30 -- Height of the floating window
       local col = math.floor((width - win_width) / 2)
       local row = math.floor((height - win_height) / 2)
@@ -41,7 +41,7 @@ return {
         style = 'minimal',
         border = 'single',
       }
-      local buf = vim.api.nvim_create_buf(false, true) -- Create an empty buffer
+      local buf = vim.api.nvim_create_buf(false, true)   -- Create an empty buffer
       local win = vim.api.nvim_open_win(buf, true, opts) -- Open a floating window with the buffer
 
       -- Open terminal in the floating window and get the job ID
@@ -57,7 +57,7 @@ return {
       vim.api.nvim_win_set_height(win, win_height)
       vim.api.nvim_buf_set_option(buf, 'filetype', 'terminal')
       vim.cmd('startinsert') -- Start insert mode for the terminal
-      vim.wo.winbar = '' -- Disable winbar in the terminal
+      vim.wo.winbar = ''     -- Disable winbar in the terminal
     end,
     rtxt = "󱁐find",
   },
@@ -162,7 +162,11 @@ return {
   {
     name = "Zen Mode",
     cmd = function()
-      vim.cmd("ZenMode")
+      require("zen-mode").toggle({
+        window = {
+          width = 0.85, -- Adjust the width as per your preference
+        },
+      })
     end,
   },
 }
